eaab7f0e8364796e617d9d34d793a0724288659f
Petr Horáček <phoracek@redhat.com>
2021-01-17 16:42:35 +0100

    Add documentation and test coverage to Math
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/modules/math.rs
+++ src/modules/math.rs
@@ -1,14 +1,19 @@
+//! This module serves as a simple calculator with up to 2 variables on its
+//! input.
+//!
+//! It can be used as a mixer, attenuator, VCO and even an oscillator.
+
+use gazpatcho::config::*;
 use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
-
-use gazpatcho::config::*;
 
 use crate::registration::{Module, ModuleInstance};
 use crate::samples::{self, Samples};
 
 pub const IN1: &str = "a";
 pub const IN2: &str = "b";
+pub const FORMULA: &str = "formula";
 
 pub struct Math;
 
@@ -48,7 +53,7 @@ where
                 },
             ],
             widgets: vec![TextBox {
-                key: "formula".to_owned(),
+                key: FORMULA.to_owned(),
                 capacity: 200,
                 size: [200.0, 23.0],
                 read_only: false,
@@ -75,9 +80,11 @@ pub struct Widget {
 
 impl crate::registration::Widget for Widget {
     fn update(&mut self, data: HashMap<String, gazpatcho::model::Value>) {
-        let formula = data.get("formula").unwrap().unwrap_string();
-        if let Ok(formula) = formula.parse() {
-            *self.formula.borrow_mut() = formula;
+        let formula = data.get(FORMULA).unwrap().unwrap_string();
+        if let Ok(formula) = formula.parse::<meval::Expr>() {
+            if formula.clone().bind2("a", "b").is_ok() {
+                *self.formula.borrow_mut() = formula;
+            }
         }
     }
 }
@@ -137,10 +144,151 @@ impl graphity::Node<Samples> for Node {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::samples;
     use graphity::Node as _;
 
     #[test]
-    fn sum2() {
+    fn instantiate_module() {
+        let math = Math;
+
+        let instance =
+            <Math as Module<Node, Consumer, Producer>>::instantiate(&math, "id".to_string());
+
+        assert!(instance.widget.is_some());
+    }
+
+    #[test]
+    fn get_template() {
+        let math = Math;
+
+        let template = <Math as Module<Node, Consumer, Producer>>::template(&math);
+
+        assert_eq!(template.class, "math");
+    }
+
+    #[test]
+    fn access_consumer_in1_by_pin_name() {
+        let math = Math;
+
+        let consumer = <Math as Module<Node, Consumer, Producer>>::consumer(&math, IN1);
+
+        assert_eq!(consumer, Consumer::In1);
+    }
+
+    #[test]
+    fn access_consumer_in2_by_pin_name() {
+        let math = Math;
+
+        let consumer = <Math as Module<Node, Consumer, Producer>>::consumer(&math, IN2);
+
+        assert_eq!(consumer, Consumer::In2);
+    }
+
+    #[test]
+    fn access_producer_by_pin_name() {
+        let math = Math;
+
+        let producer =
+            <Math as Module<Node, Consumer, Producer>>::producer(&math, "does_not_matter");
+
+        assert_eq!(producer, Producer);
+    }
+
+    #[test]
+    fn return_0_after_initialization() {
+        let math = Math;
+        let instance =
+            <Math as Module<Node, Consumer, Producer>>::instantiate(&math, "id".to_string());
+        let mut node = instance.node;
+
+        node.tick();
+
+        assert_eq!(node.read(Producer)[0], 0.0);
+    }
+
+    fn set_formula(widget: &mut dyn crate::registration::Widget, formula: &str) {
+        let data: HashMap<_, _> = [(
+            FORMULA.to_string(),
+            gazpatcho::model::Value::String(formula.to_string()),
+        )]
+        .iter()
+        .cloned()
+        .collect();
+        widget.update(data);
+    }
+
+    #[test]
+    fn update_to_a_different_formula() {
+        let math = Math;
+        let instance =
+            <Math as Module<Node, Consumer, Producer>>::instantiate(&math, "id".to_string());
+        let mut node = instance.node;
+        let mut widget = instance.widget.unwrap();
+
+        set_formula(&mut *widget, "a + b");
+
+        node.write(Consumer::In1, samples::value(1.0));
+        node.write(Consumer::In2, samples::value(2.0));
+        node.tick();
+
+        assert_eq!(node.read(Producer)[0], 3.0);
+    }
+
+    #[test]
+    fn return_0_when_set_blank() {
+        let math = Math;
+        let instance =
+            <Math as Module<Node, Consumer, Producer>>::instantiate(&math, "id".to_string());
+        let mut node = instance.node;
+        let mut widget = instance.widget.unwrap();
+
+        set_formula(&mut *widget, "");
+
+        node.write(Consumer::In1, samples::value(1.0));
+        node.write(Consumer::In2, samples::value(2.0));
+        node.tick();
+
+        assert_eq!(node.read(Producer)[0], 0.0);
+    }
+
+    #[test]
+    fn keep_previous_formula_when_the_new_one_has_invalid_syntax() {
+        let math = Math;
+        let instance =
+            <Math as Module<Node, Consumer, Producer>>::instantiate(&math, "id".to_string());
+        let mut node = instance.node;
+        let mut widget = instance.widget.unwrap();
+
+        set_formula(&mut *widget, "a + b");
+        set_formula(&mut *widget, "a ) 10");
+
+        node.write(Consumer::In1, samples::value(1.0));
+        node.write(Consumer::In2, samples::value(2.0));
+        node.tick();
+
+        assert_eq!(node.read(Producer)[0], 3.0);
+    }
+
+    #[test]
+    fn keep_previous_formula_when_the_new_one_has_nonexistent_variables() {
+        let math = Math;
+        let instance =
+            <Math as Module<Node, Consumer, Producer>>::instantiate(&math, "id".to_string());
+        let mut node = instance.node;
+        let mut widget = instance.widget.unwrap();
+
+        set_formula(&mut *widget, "a + b");
+        set_formula(&mut *widget, "a + c");
+
+        node.write(Consumer::In1, samples::value(1.0));
+        node.write(Consumer::In2, samples::value(2.0));
+        node.tick();
+
+        assert_eq!(node.read(Producer)[0], 3.0);
+    }
+
+    #[test]
+    fn sum_two_values() {
         let formula = Rc::new(RefCell::new("a + b".parse().unwrap()));
 
         let mut math = Node::new(formula);
