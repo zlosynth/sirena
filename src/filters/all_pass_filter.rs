82c81af85302761ed580b3f89fffdb6a28cc00c1
Petr Horáček <phoracek@redhat.com>
2021-01-26 22:32:44 +0100

    Allow dynamic changes of all pass filter config
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/filters/all_pass_filter.rs
+++ src/filters/all_pass_filter.rs
@@ -1,28 +1,39 @@
 use super::ring_buffer::RingBuffer;
 
 pub struct AllPassFilter {
-    parameters: AllPassFilterParameters,
+    gain: f32,
+    delay: usize,
     buffer: RingBuffer,
 }
 
-pub struct AllPassFilterParameters {
-    gain: f32,
-    delay: usize,
-}
-
 impl AllPassFilter {
-    pub fn new(parameters: AllPassFilterParameters) -> Self {
+    pub fn new() -> Self {
         Self {
-            parameters,
+            gain: 0.0,
+            delay: 0,
             buffer: RingBuffer::new(),
         }
     }
 
+    pub fn set_gain(&mut self, gain: f32) -> &mut Self {
+        assert!(
+            gain >= 0.0 && gain < 1.0,
+            "gain must be set between 0 and 1"
+        );
+        self.gain = gain;
+        self
+    }
+
+    pub fn set_delay(&mut self, delay: usize) -> &mut Self {
+        self.delay = delay;
+        self
+    }
+
     pub fn process(&mut self, data: &mut [f32]) {
         for x in data.iter_mut() {
-            let feed_forward_data = *x * -self.parameters.gain;
-            let delayed_data = self.buffer.peek(-(self.parameters.delay as i32) + 1);
-            let feed_back_data = (feed_forward_data + delayed_data) * self.parameters.gain;
+            let feed_forward_data = *x * -self.gain;
+            let delayed_data = self.buffer.peek(-(self.delay as i32) + 1);
+            let feed_back_data = (feed_forward_data + delayed_data) * self.gain;
             self.buffer.write(feed_back_data + *x);
             *x = feed_forward_data + delayed_data;
         }
@@ -35,19 +46,38 @@ mod tests {
 
     #[test]
     fn initialize_filter() {
-        let _all_pass_filter = AllPassFilter::new(AllPassFilterParameters {
-            gain: 0.5,
-            delay: 2,
-        });
+        let _all_pass_filter = AllPassFilter::new();
+    }
+
+    #[test]
+    fn set_gain() {
+        let mut all_pass_filter = AllPassFilter::new();
+
+        all_pass_filter.set_gain(0.3);
+    }
+
+    #[test]
+    #[should_panic(expected = "gain must be set between 0 and 1")]
+    fn set_invalid_gain_above_the_limit() {
+        let mut all_pass_filter = AllPassFilter::new();
+
+        all_pass_filter.set_gain(10.0);
+    }
+
+    #[test]
+    #[should_panic(expected = "gain must be set between 0 and 1")]
+    fn set_invalid_gain_below_the_limit() {
+        let mut all_pass_filter = AllPassFilter::new();
+
+        all_pass_filter.set_gain(-0.3);
     }
 
     #[test]
     fn process_data() {
-        let mut all_pass_filter = AllPassFilter::new(AllPassFilterParameters {
-            gain: 0.5,
-            delay: 2,
-        });
         let mut data = [8.0, 0.0, 8.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+
+        let mut all_pass_filter = AllPassFilter::new();
+        all_pass_filter.set_gain(0.5).set_delay(2);
 
         all_pass_filter.process(&mut data);
 
