1a624f3bb68ba8d6b69196e016fabdbab2d7fc0f
Petr Horáček <phoracek@redhat.com>
2021-02-07 20:33:35 +0100

    Rename BUFFER_SIZE to MAX_BUFFER_SIZE
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- modules/src/ring_buffer.rs
+++ modules/src/ring_buffer.rs
@@ -1,21 +1,21 @@
-const BUFFER_SIZE: usize = 48000;
+pub const MAX_BUFFER_SIZE: usize = 48000;
 
 pub struct RingBuffer {
-    buffer: [f32; BUFFER_SIZE],
+    buffer: [f32; MAX_BUFFER_SIZE],
     write_index: usize,
 }
 
 impl RingBuffer {
     pub fn new() -> Self {
         let buffer = {
-            let mut data: [std::mem::MaybeUninit<f32>; BUFFER_SIZE] =
+            let mut data: [std::mem::MaybeUninit<f32>; MAX_BUFFER_SIZE] =
                 unsafe { std::mem::MaybeUninit::uninit().assume_init() };
             for elem in &mut data[..] {
                 unsafe {
                     std::ptr::write(elem.as_mut_ptr(), 0.0);
                 }
             }
-            unsafe { std::mem::transmute::<_, [f32; BUFFER_SIZE]>(data) }
+            unsafe { std::mem::transmute::<_, [f32; MAX_BUFFER_SIZE]>(data) }
         };
 
         Self {
@@ -25,14 +25,14 @@ impl RingBuffer {
     }
 
     pub fn write(&mut self, value: f32) {
-        self.write_index %= BUFFER_SIZE;
+        self.write_index %= MAX_BUFFER_SIZE;
         self.buffer[self.write_index] = value;
         self.write_index += 1;
     }
 
     pub fn peek(&self, relative_index: i32) -> f32 {
         let index = (self.write_index as i32 + relative_index - 1)
-            .wrapping_rem_euclid(BUFFER_SIZE as i32) as usize;
+            .wrapping_rem_euclid(MAX_BUFFER_SIZE as i32) as usize;
         self.buffer[index]
     }
 }
@@ -69,7 +69,7 @@ mod tests {
     fn write_over_capacity() {
         let mut buffer = RingBuffer::new();
 
-        for x in 0..=(BUFFER_SIZE + 1) {
+        for x in 0..=(MAX_BUFFER_SIZE + 1) {
             buffer.write(x as f32);
         }
     }
@@ -77,11 +77,11 @@ mod tests {
     #[test]
     fn cross_buffer_end_while_reading() {
         let mut buffer = RingBuffer::new();
-        for x in 0..=(BUFFER_SIZE) {
+        for x in 0..=(MAX_BUFFER_SIZE) {
             buffer.write(x as f32);
         }
 
-        assert_eq!(buffer.peek(0) as usize, BUFFER_SIZE);
-        assert_eq!(buffer.peek(-1) as usize, BUFFER_SIZE - 1);
+        assert_eq!(buffer.peek(0) as usize, MAX_BUFFER_SIZE);
+        assert_eq!(buffer.peek(-1) as usize, MAX_BUFFER_SIZE - 1);
     }
 }
