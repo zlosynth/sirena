485a04fe0099b4c72eb163c191b4bc8acfcfdc5e
Petr Horáček <phoracek@redhat.com>
2021-02-14 19:30:41 +0100

    Support no_std usage
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- modules/src/lib.rs
+++ modules/src/lib.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(feature = "std"), no_std)]
 #![allow(clippy::new_without_default)]
 
 #[cfg(test)]
@@ -8,5 +9,7 @@ pub mod all_pass_filter;
 pub mod comb_filter;
 pub mod delay;
 pub mod ring_buffer;
-pub mod spectral_analysis;
 pub mod wavetable_oscillator;
+
+#[cfg(feature = "std")]
+pub mod spectral_analysis;
