05a7a8cd5d27b2edaedb0ae79bb66fc9116b09cf
Petr Horáček <phoracek@redhat.com>
2021-02-23 21:07:24 +0100

    Cartesian: Set frequency
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- src/cartesian.rs
+++ src/cartesian.rs
@@ -15,6 +15,11 @@ impl Cartesian {
         Self { oscillator }
     }
 
+    pub fn set_frequency(&mut self, frequency: f32) -> &mut Self {
+        self.oscillator.set_frequency(frequency);
+        self
+    }
+
     pub fn tick(&mut self) -> f32 {
         self.oscillator.tick()
     }
@@ -40,5 +45,37 @@ mod tests {
 
         assert_relative_eq!(value_1, 0.0, epsilon = 0.1);
         assert!(value_2 > value_1);
+    }
+
+    #[test]
+    fn set_frequency() {
+        const SAMPLE_RATE: u32 = 48000;
+
+        let delta_one_tick_frequency_200 = {
+            let mut cartesian = Cartesian::new(SAMPLE_RATE);
+            cartesian.set_frequency(200.0);
+
+            let original = cartesian.tick();
+            let updated = cartesian.tick();
+
+            updated - original
+        };
+
+        let delta_two_ticks_frequency_100 = {
+            let mut cartesian = Cartesian::new(SAMPLE_RATE);
+            cartesian.set_frequency(100.0);
+
+            let original = cartesian.tick();
+            cartesian.tick();
+            let updated = cartesian.tick();
+
+            updated - original
+        };
+
+        assert_relative_eq!(
+            delta_one_tick_frequency_200,
+            delta_two_ticks_frequency_100,
+            max_relative = 0.001
+        );
     }
 }
