7318da12a8de2f6bd19f0e44cec1539efc5ff10a
Petr Horáček <phoracek@redhat.com>
2021-02-26 19:11:45 +0100

    Cartesian: Add a benchmark
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- README.md
+++ README.md
@@ -10,5 +10,20 @@ make
 
 # run slow tests too
 make SLOW=1
+
+# run benchmark
+cargo bench
+
+# profiling example
+rm -f target/release/deps/bench-* callgrind.out.*
+cargo bench --no-run
+BENCH=$(find target/release/deps -type f -executable -name 'bench-*')
+valgrind \
+    --tool=callgrind \
+    --dump-instr=yes \
+    --collect-jumps=yes \
+    --simulate-cache=yes \
+    ${BENCH} --bench --profile-time 10
+kcachegrind callgrind.out.*
 ```
 
---
+++ benches/bench.rs
@@ -0,0 +1,30 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+use sirena::cartesian::Cartesian;
+use sirena::wavetable_oscillator::Wavetable;
+use sirena::wavetable_oscillator::{saw, sine, triangle};
+
+pub fn cartesian_play(cartesian: &mut Cartesian, buffer: &mut [f32]) {
+    for x in buffer.iter_mut() {
+        *x = cartesian.tick();
+    }
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("cartesian", |b| {
+        const SAMPLE_RATE: u32 = 48000;
+        let wavetable_a = Wavetable::new(sine(), SAMPLE_RATE);
+        let wavetable_b = Wavetable::new(triangle(), SAMPLE_RATE);
+        let wavetable_c = Wavetable::new(saw(), SAMPLE_RATE);
+        let mut cartesian = Cartesian::new(&wavetable_a, &wavetable_b, &wavetable_c, SAMPLE_RATE);
+        cartesian
+            .set_frequency(440.0)
+            .set_enabled_voices(7)
+            .set_detune(2.0);
+        let mut buffer = [0.0; SAMPLE_RATE as usize];
+        b.iter(|| cartesian_play(black_box(&mut cartesian), black_box(&mut buffer)));
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
