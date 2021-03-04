cbe2e50c61dcc935e708af648f223f22c10b57ff
Petr Horáček <phoracek@redhat.com>
2021-03-04 20:33:23 +0100

    Osc: Fix benchmark refering to old name
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,26 +1,25 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use sirena::cartesian::Cartesian;
+use sirena::osc1::Osc1;
 use sirena::wavetable_oscillator::Wavetable;
 use sirena::wavetable_oscillator::{saw, sine, triangle};
 
-pub fn cartesian_play(cartesian: &mut Cartesian, buffer: &mut [f32]) {
-    cartesian.populate(buffer);
+pub fn osc1_play(osc1: &mut Osc1, buffer: &mut [f32]) {
+    osc1.populate(buffer);
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("cartesian", |b| {
+    c.bench_function("osc1", |b| {
         const SAMPLE_RATE: u32 = 48000;
         let wavetable_a = Wavetable::new(sine(), SAMPLE_RATE);
         let wavetable_b = Wavetable::new(triangle(), SAMPLE_RATE);
         let wavetable_c = Wavetable::new(saw(), SAMPLE_RATE);
-        let mut cartesian = Cartesian::new(&wavetable_a, &wavetable_b, &wavetable_c, SAMPLE_RATE);
-        cartesian
-            .set_frequency(440.0)
+        let mut osc1 = Osc1::new(&wavetable_a, &wavetable_b, &wavetable_c, SAMPLE_RATE);
+        osc1.set_frequency(440.0)
             .set_enabled_voices(7)
             .set_detune(2.0);
         let mut buffer = [0.0; 64];
-        b.iter(|| cartesian_play(black_box(&mut cartesian), black_box(&mut buffer)));
+        b.iter(|| osc1_play(black_box(&mut osc1), black_box(&mut buffer)));
     });
 }
 
