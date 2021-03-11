abba1938f8db1e8e813d2a29df91a273a9edaaa8
Petr Horáček <phoracek@redhat.com>
2021-03-11 22:40:34 +0100

    Add benchmarks for F32 bound functions
    Signed-off-by: Petr Horáček <phoracek@redhat.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,7 +1,9 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 use sirena::osc1::Osc1;
+use sirena::signal;
 use sirena::taper;
+use sirena::tone;
 use sirena::wavetable_oscillator::Wavetable;
 use sirena::wavetable_oscillator::{saw, sine, triangle};
 
@@ -25,6 +27,18 @@ fn criterion_benchmark(c: &mut Criterion) {
 
     c.bench_function("log_taper", |b| {
         b.iter(|| taper::log(black_box(0.5)));
+    });
+
+    c.bench_function("saw_wavetable", |b| {
+        b.iter(|| Wavetable::new(black_box(saw()), black_box(44100)));
+    });
+
+    c.bench_function("tone", |b| {
+        b.iter(|| tone::detune_frequency(black_box(440.0), black_box(2.0)));
+    });
+
+    c.bench_function("normalize", |b| {
+        b.iter(|| signal::normalize(&mut [0.0, -1.0, 2.0]));
     });
 }
 
