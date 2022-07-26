d3e398bfc8054ba02db25e43adcdc85bf1f3833d
Petr Horacek <hrck@protonmail.com>
2022-07-26 19:20:16 +0200

    Add release script
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

---
+++ hack/release.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+version=${1}
+
+sed -i "s/## Unreleased/## Unreleased\n\n## ${version}/" CHANGELOG.md
+find -name Cargo.toml | xargs sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/"
