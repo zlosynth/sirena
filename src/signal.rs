0c84f48182651551cf06a6970b15191cee84e1c2
Petr Horacek <hrck@protonmail.com>
2022-08-01 21:17:55 +0200

    Create signal from iterator
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- src/signal.rs
+++ src/signal.rs
@@ -4,6 +4,8 @@
 //! [dasp-signal](https://github.com/RustAudio/dasp) except this implementation
 //! supports `#[no_std]` on stable.
 
+const EQUILIBRIUM: f32 = 0.0;
+
 /// Types that yield values of a PCM signal.
 pub trait Signal {
     fn next(&mut self) -> f32;
@@ -40,6 +42,42 @@ where
     }
 }
 
+/// Create a new `Signal` from the given `Frame`-yielding `Iterator`.
+pub fn from_iter<I>(frames: I) -> FromIterator<I::IntoIter>
+where
+    I: IntoIterator<Item = f32>,
+{
+    let mut iter = frames.into_iter();
+    let next = iter.next();
+    FromIterator { iter, next }
+}
+
+/// A type that wraps an Iterator and provides a `Signal` implementation for it.
+#[derive(Clone)]
+pub struct FromIterator<I>
+where
+    I: Iterator,
+{
+    iter: I,
+    next: Option<I::Item>,
+}
+
+impl<I> Signal for FromIterator<I>
+where
+    I: Iterator<Item = f32>,
+{
+    #[inline]
+    fn next(&mut self) -> f32 {
+        match self.next.take() {
+            Some(frame) => {
+                self.next = self.iter.next();
+                frame
+            }
+            None => EQUILIBRIUM,
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,6 +106,14 @@ mod tests {
     #[test]
     fn given_clip_amp_when_signal_is_outside_limit_it_gets_clipped() {
         let mut signal = StableSignal(2.0).clip_amp(1.0);
+        assert_relative_eq!(signal.next(), 1.0);
+    }
+
+    #[test]
+    fn given_from_iter_when_called_on_an_iterator_it_returns_signal() {
+        let array = [0.0, 1.0, 2.0];
+        let mut signal = from_iter(array.into_iter());
+        assert_relative_eq!(signal.next(), 0.0);
         assert_relative_eq!(signal.next(), 1.0);
     }
 }
