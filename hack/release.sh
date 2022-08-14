e6138e8bb4daf90bf9ed45ac402ea4c658f451de
Petr Horacek <hrck@protonmail.com>
2022-08-14 10:51:46 +0200

    Run checks after a release is marked
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -5,3 +5,5 @@ version=${1}
 
 sed -i "s/## Unreleased/## Unreleased\n\n## ${version}/" CHANGELOG.md
 find -name Cargo.toml | xargs sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/"
+
+cargo make dev-slow
